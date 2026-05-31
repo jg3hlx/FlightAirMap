7f76d517db4b9668240f41d8447d682f6ce3b3cb
JG3HLX <76939165+jg3hlx@users.noreply.github.com>
2026-06-01 07:50:31 +0900

    Fix formatting in conditional for weather stations

--- scripts/daemon-spotter.php
+++ scripts/daemon-spotter.php
@@ -2013,7 +2013,7 @@ while ($i > 0) {
 					} else {
 						$Source->addLocation($data['ident'],$data['latitude'],$data['longitude'],$data['altitude'],'','',$data['source_name'],'antenna.png','gs',$id,0,$data['datetime']);
 					}
-				    } elseif ((isset($line['symbol']) && $line['symbol'] === 'Weather Station') || (isset($line['symbol']) && $line['symbol'] === 'National Weather Service Site'))　{
+				    } elseif ((isset($line['symbol']) && $line['symbol'] === 'Weather Station') || (isset($line['symbol']) && $line['symbol'] === 'National Weather Service Site')){
 					//if ($globalDebug) echo '!! Weather Station not yet supported'."\n";
 					if ($globalDebug) echo '# Weather Station added'."\n";
 					$Source->deleteOldLocationByType('wx');
