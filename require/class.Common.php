cb0219ce81435b48bcf433a2f26ec367fdef145d
JG3HLX <76939165+jg3hlx@users.noreply.github.com>
2026-06-01 09:02:44 +0900

    PHP8

--- require/class.Common.php
+++ require/class.Common.php
@@ -756,7 +756,7 @@ class Common {
 	public function getUserIP() { 
 		$client = @$_SERVER['HTTP_CLIENT_IP'];
 		$forward = @$_SERVER['HTTP_X_FORWARDED_FOR'];
-		return filter_var($client, FILTER_VALIDATE_IP) ? $client : (filter_var($forward, FILTER_VALIDATE_IP) ? $forward : $_SERVER['REMOTE_ADDR']);
+		return filter_var($client, FILTER_VALIDATE_IP)  ? $client : (filter_var($forward, FILTER_VALIDATE_IP) ? $forward : $_SERVER['REMOTE_ADDR']);
 	}
 	public function replace_mb_substr($string, $offset, $length)
 	{
